979ddbbc190c719359efaebe3da51ca99314ebeb
Gabor Barna <gbarna@whitepages.com>
2016-08-06 21:45:33 +0200

    use mount.core for server start/stop

--- build.boot
+++ build.boot
@@ -13,11 +13,14 @@
                  [compojure "1.5.1"]
                  [hiccup "1.0.5"]
                  [garden "1.3.2"]
-                 [org.clojure/core.match "0.3.0-alpha4"]])
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [mount "0.1.10"]])
 
 (require
+ '[ubik.core]
  '[adzerk.boot-cljs :refer [cljs]]
- '[adzerk.boot-reload :refer [reload]])
+ '[adzerk.boot-reload :refer [reload]]
+ '[mount.core :refer [start stop] :as mount])
 
 (deftask dev
   []
--- src/clj/ubik/core.clj
+++ src/clj/ubik/core.clj
@@ -4,38 +4,39 @@
              [event :refer [event-msg-handler]]]
             [ubik
              [sente :refer [ch-chsk]]
-             [scheduler :refer [start-scheduler!]]]
+             [scheduler :refer [start-scheduler! stop-scheduler!]]]
+            [clojure.edn :as edn]
+            [mount.core :refer [defstate] :as mount]
             [taoensso.timbre :refer [debugf]]
             [taoensso.sente :as sente]
             [org.httpkit.server :as http-kit]))
 
-(defn start-web-server!* [ring-handler port]
-  (let [http-kit-stop-fn (http-kit/run-server ring-handler {:port port})]
-    {:server nil
-     :port (:local-port (meta http-kit-stop-fn))
-     :stop-fn (fn [] (http-kit-stop-fn :timeout 100))}))
+(defn start-router! []
+  (sente/start-chsk-router! ch-chsk event-msg-handler))
 
-(defonce web-server (atom nil))
-(defn stop-web-server! [] (when-let [m @web-server] ((:stop-fn m))))
-(defn start-web-server! [& [port]]
-  (stop-web-server!)
-  (let [{:keys [stop-fn port] :as server-map} (start-web-server!* (var ubik-ring-handler) (or port 0))
+(defn start-web-server! [{:keys [port] :or {port 3000}}]
+  (let [stop-fn (http-kit/run-server #'ubik-ring-handler {:port port})
         uri (format "http://localhost:%s/" port)]
-    (debugf "uri: `%s`" uri)
+    (debugf "uri: %s" uri)
     (try
       (.browse (java.awt.Desktop/getDesktop) (java.net.URI. uri))
       (catch java.awt.HeadlessException _))
-    (reset! web-server server-map)))
+    stop-fn))
 
-(defonce router (atom nil))
-(defn stop-router! [] (when-let [stop-f @router] (stop-f)))
-(defn start-router! []
-  (stop-router!)
-  (reset! router (sente/start-chsk-router! ch-chsk event-msg-handler)))
+(defstate scheduler
+  :start (start-scheduler!)
+  :stop (stop-scheduler!))
 
-(defn start! []
-  (start-router!)
-  (start-web-server! 3000)
-  (start-scheduler!))
+(defstate router
+  :start (start-router!)
+  :stop (router))
 
-(start!)
+(defstate web-server
+  :start (start-web-server! (mount/args))
+  :stop (web-server :timeout 100))
+
+(defn mount-args [[port]]
+  {:port (edn/read-string port)})
+
+(defn -main [& args]
+  (mount/start-with-args (mount-args args)))
