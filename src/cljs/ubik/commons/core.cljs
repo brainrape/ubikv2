431128fdeed4fca629672b2d43bee26e8562ccfd
Gabor Barna <gbarna@whitepages.com>
2016-08-25 23:35:48 +0200

    set nr of anims

--- src/cljs/ubik/commons/core.cljs
+++ src/cljs/ubik/commons/core.cljs
@@ -3,8 +3,8 @@
            [taoensso.sente :as sente]))
 
 (def anim-ids
-  (let [face-anim-ids (into (sorted-map) (map (fn [type] [type (range 12)]) [:top :center :bottom]))
-        bg-anim-ids (range 4)]
+  (let [face-anim-ids (into (sorted-map) (map (fn [type] [type (range 20)]) [:top :center :bottom]))
+        bg-anim-ids (range 3)]
     (assoc face-anim-ids :bg bg-anim-ids)))
 
 (def face-anim-types (into #{} (keys (dissoc anim-ids :bg))))
